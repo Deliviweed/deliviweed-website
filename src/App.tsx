63b7cc666426d0c86059f3124247a77fb97e56e4
Bryant MacMahon <bryantmac@gmail.com>
2023-08-25 10:52:28 -0700

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Deliviweed</h1>
+        <h1>- Deliviweed -</h1>
       </header>
     </div>
   );

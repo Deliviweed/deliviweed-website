0ae01274c8080ee3080b3eb367ef82015a8c28ed
Bryant MacMahon <bryantmac@gmail.com>
2023-08-25 19:55:31 -0700

    abstraction down to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/nav/Header";
+import Hero from "./pages/Hero";
+import About from "./pages/About";
+import Roadmap from "./pages/Roadmap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Deliviweed</h1>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Roadmap />
+    </>
   );
 }
 
